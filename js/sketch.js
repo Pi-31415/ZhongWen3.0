cd0c391ac5bb06e2e1bf11e5dced9e15b8b555cd
Pi <paingthetko.pi@gmail.com>
2019-04-22 20:10:27 +0800

    There is still some issue with p5 screen lock.

--- js/sketch.js
+++ js/sketch.js
@@ -1,34 +1,33 @@
+if (is_p5_on == true) {
+  var sketch = function(p) {
+    p.setup = function() {
+      var wid = p.displayWidth;
+      var hid = p.displayHeight;
+      p.createCanvas(wid, hid);
+      p.strokeWeight(5)
+      p.stroke(0);
+    }
+    p.touchMoved = function() {
+      p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
+      return false;
+    }
 
-var sketch = function(p) {
-  p.setup = function() {
-    var wid = p.displayWidth;
-    var hid = p.displayHeight;
-    p.createCanvas(wid, hid);
-    p.strokeWeight(5)
-    p.stroke(0);
-  }
-
-  p.touchMoved = function() {
-    p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
-    return false;
-  }
-
-  p.clrscr = function() {
+    p.clrscr = function() {
       p.clear();
       return false;
-  }
-
-};
-
-if(is_p5_on == true){
+    }
+  };
   new p5(sketch, 'p5canvas');
+}
+else{
+  console.log("Destroying P5 Canvas");
+  remove();
 }
 
 //Clear function, basically reloads the page
 function clearscreen() {
-
-    //Needs some way to store current mandarin word ID
-
-    switchpage('write');
-    is_p5_on = true;
+  //Needs some way to store current mandarin word ID
+  console.log("Turning on p5");
+  is_p5_on = true;
+  switchpage('write');
 }
