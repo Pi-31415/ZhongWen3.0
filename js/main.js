6b3c54c78d931161fd61dde001e1c180f807bb64
Pi <paingthetko.pi@gmail.com>
2019-04-22 11:28:04 +0800

    Basic Template Built

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 //Set all styling to material
 ons.platform.select('android');
 
-const togglemenu = () => {
+//Menu open Function
+function togglemenu() {
   document.querySelector('#menu').open();
-};
+}
+//Function to Switch Page
+function switchpage(pagename) {
+  var menu = document.getElementById('menu');
+  var page = pagename + ".html";
+  const navigator = document.querySelector('#navigator');
+  navigator.resetToPage(page).then(menu.close.bind(menu));;
+}
