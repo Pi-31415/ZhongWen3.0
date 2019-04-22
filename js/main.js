3bbff60cf2fac6a2ce5c88868de51dc54f98a00c
Pi <paingthetko.pi@gmail.com>
2019-04-22 11:41:16 +0800

    Responsivevoice enabled

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 //Set all styling to material
 ons.platform.select('android');
 
+
+
+//Settings as local variables
+
 //Menu open Function
 function togglemenu() {
   document.querySelector('#menu').open();
@@ -11,4 +15,21 @@ function switchpage(pagename) {
   var page = pagename + ".html";
   const navigator = document.querySelector('#navigator');
   navigator.resetToPage(page).then(menu.close.bind(menu));;
+}
+
+//Responsive Voice Functions
+function speakeng(k) {
+    speaker = "UK English Female";
+    speed = 1.00;
+    responsiveVoice.speak(k, speaker, {
+        rate: speed
+    });
+}
+
+function speakchin(k) {
+    speaker = "Chinese Female";
+    speed = 1.00;
+    responsiveVoice.speak(k, speaker, {
+        rate: speed
+    });
 }
